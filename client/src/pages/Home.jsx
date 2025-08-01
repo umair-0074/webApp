200a0ea3360aa7059f0f763c5951da0d2878a113
umair-0074 <umairyousaf.dev>
2025-08-01 08:21:10 +0500

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../component/Header.jsx";
+import Footer from "../component/Footer.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Home
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
