0df9f6ebfe4cb5a9dc2eed4e164692454f3754c5
umair-0074 <umairyousaf.dev>
2025-09-02 20:21:31 +0500

    email

--- server/src/controllers/messageController.js
+++ server/src/controllers/messageController.js
@@ -1,4 +1,5 @@
 import { createMessage } from "../models/messagModel.js";
+import nodemailer from "nodemailer";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -14,6 +15,21 @@ export const sendMessage = async (req, res) => {
 
     // Save to DB
     const newMessage = await createMessage({ name, email, subject, message });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail", // or use SMTP
+      auth: {
+        user: process.env.EMAIL_USER, // your email
+        pass: process.env.EMAIL_PASS, // your email password / app password
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER, // send to yourself
+      subject: `📩 New Message: ${subject}`,
+      text: `You received a new message:\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
 
     //  Success response
     return res.status(201).json({
